b8b4b5fc7edbacbf521a7e58b78fbec5db346aed
bashbashbash <7basch@gmail.com>
2024-02-25 09:05:55 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, DxTextBoxModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
