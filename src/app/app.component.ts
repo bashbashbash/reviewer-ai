5ff0a1387c12664d70f3a8bc4ead7774d536e5d4
bashbashbash <7basch@gmail.com>
2024-02-25 09:00:48 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div id="container">
+    <dx-text-box 
+        [(value)]="value"
+        mode="url"
+        label="Link"
+        labelMode="floating"
+        [maxLength]="20"
+        [showClearButton]="true"
+        (onEnterKey)="onEnterKey()"
+    >
+    </dx-text-box>
+</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  value: string = "";
+  onEntryKey() {
+    console.log(this.value)
+  }
 }
