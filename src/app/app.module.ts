e5257e3a1181d65a086a0951c4fc0339e5edc4e1
bashbashbash <7basch@gmail.com>
2024-02-25 08:47:09 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModel } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
