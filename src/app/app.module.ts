7d8af7997adbe051f29937a9cc1edbfcdb9727b0
bashbashbash <7basch@gmail.com>
2024-02-25 22:46:20 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { HttpClientModule} from '@angular/common/http';
-import { DxFormModule} from 'devextreme-angular';
+import { DxFormModule, DxTextBoxModule } from 'devextreme-angular';
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, DxFormModule],
+  imports: [BrowserModule, HttpClientModule, DxFormModule, DxTextBoxModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
